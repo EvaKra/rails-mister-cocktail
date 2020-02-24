8def51e8d3f31aa57ef04f70d87e1b80d68139d3
eva kramer <eva.kramer91@gmail.com>
2020-02-24 11:11:40 +0000

    to be continued

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,15 +1,18 @@
 class DosesController < ApplicationController
   def new
-    @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @dose.save
+    if @dose.save
     redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
