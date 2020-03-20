2bc956ce168887041fc3f8fef0403c0db29e9842
eva kramer <eva.kramer91@gmail.com>
2020-03-20 11:42:30 +0000

    index cards with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,17 +19,44 @@ puts "creating new Ingredients"
  Ingredient.create(name: "brown sugar")
 
 puts "creating new Cocktails"
-file = URI.open("https://images.unsplash.com/photo-1570036394455-f3f3f281e403?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1065&q=80")
+file = URI.open("https://images.unsplash.com/photo-1562890042-cb70b22b294d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80rai")
 cocktail = Cocktail.new(name: "Mojito")
 cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
 cocktail.save
-#  cocktails = [{name: "Mojito", url: "https://images.unsplash.com/photo-1570036394455-f3f3f281e403?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1065&q=80"}, {name: "Canchanchara", url: "https://www.mixolopedia.com/wp-content/uploads/2018/03/canchanchara-cocktail.jpg"}]
 
+file = URI.open("https://www.mixolopedia.com/wp-content/uploads/2018/03/canchanchara-cocktail.jpg")
+cocktail = Cocktail.new(name: "Canchanchara")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
 
-#  cocktails.each do |cocktail|
-#     cocktail = Cocktail.new(name: cocktail[:name])
-#     file = URI.open(cocktail[:url])
-#     cocktail.photo.attach(io: file, filename: "cocktail.png", content_type: 'image/png')
-#     cocktail.save
-#   end
+file = URI.open("https://images.unsplash.com/photo-1490324120634-0fa86c62d6c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=759&q=80")
+cocktail = Cocktail.new(name: "Pina Colada")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
+
+file = URI.open("https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/05/lime-daiquiri.jpg?itok=zFJY6Trp")
+cocktail = Cocktail.new(name: "Daiquiri")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
+
+file = URI.open("https://noseychef.com/wp-content/uploads/2018/07/IMG_3791.jpg")
+cocktail = Cocktail.new(name: "Cuba Libre")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
+
+file = URI.open("https://images.unsplash.com/photo-1560963689-b5682b6440f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=986&q=80")
+cocktail = Cocktail.new(name: "Havana Loco")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
+
+file = URI.open("https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=986&q=80")
+cocktail = Cocktail.new(name: "Guantanamera")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
+
+file = URI.open("https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2018/04/spiced-bloody-mary-shots-f4.jpg?itok=y5h158JM")
+cocktail = Cocktail.new(name: "Cubanito")
+cocktail.photo.attach(io: file, filename: 'cocktail.png', content_type: 'image/png')
+cocktail.save
+
 puts "Hooray"
